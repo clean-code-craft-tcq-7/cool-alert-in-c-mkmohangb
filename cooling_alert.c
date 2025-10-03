85597880058926a9dfc8122b443ed8fb4ebd0f01
Manoj Mohan <haimanojkumar@gmail.com>
2025-10-03 12:56:16 +0530

    minimal code to pass the tests

--- cooling_alert.c
+++ cooling_alert.c
@@ -4,7 +4,7 @@
 // ----------- STEP 2 -----------------
 // Minimal code to compile and fail the test in STEP 2 of test_cooling_alert.cpp
 
-// const float PASSIVE_COOLING_LIMIT = 40.0;
+const float PASSIVE_COOLING_LIMIT = 40.0;
 // const float HI_ACTIVE_COOLING_LIMIT = 55.0;
 
 Action battery_temperature_to_action(CoolingType cooling_type, float temperature) {
@@ -12,20 +12,20 @@ Action battery_temperature_to_action(CoolingType cooling_type, float temperature
     // ------- STEP 3 -----------------
     // Minimal code to pass tests in test_cooling_alert.cpp
 
-    // if (cooling_type == PASSIVE_COOLING && temperature > PASSIVE_COOLING_LIMIT) {
+    if (cooling_type == PASSIVE_COOLING && temperature > PASSIVE_COOLING_LIMIT) {
+        action.actionType = ALERT_EMAIL;
+        snprintf(action.actionBody, sizeof(action.actionBody),
+            "Temperature alert: %.1fF", temperature);
+    // Step 4: duplicated logic for active cooling type
+    // } else if (cooling_type == HI_ACTIVE_COOLING && temperature > HI_ACTIVE_COOLING_LIMIT) {
     //     action.actionType = ALERT_EMAIL;
     //     snprintf(action.actionBody, sizeof(action.actionBody),
     //         "Temperature alert: %.1fF", temperature);
-    // // Step 4: duplicated logic for active cooling type
-    // // } else if (cooling_type == HI_ACTIVE_COOLING && temperature > HI_ACTIVE_COOLING_LIMIT) {
-    // //     action.actionType = ALERT_EMAIL;
-    // //     snprintf(action.actionBody, sizeof(action.actionBody),
-    // //         "Temperature alert: %.1fF", temperature);
-    // // Step 4 code ends -------
-    // } else {
-    //     action.actionType = NO_ALERT;
-    //     action.actionBody[0] = '\0';
-    // }
+    // Step 4 code ends -------
+    } else {
+        action.actionType = NO_ALERT;
+        action.actionBody[0] = '\0';
+    }
 
     //------- STEP 3 code ends -------
     return action;
